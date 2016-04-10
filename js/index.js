721759da56840d8be4d9ba5f1c2721e7856d84bb
Olivier Midy <osmidy@mit.edu>
2016-04-10 17:39:51 -0400

    Changes to index, mainLayout files

--- js/index.js
+++ js/index.js
@@ -34,16 +34,33 @@ function populatePage() {
  * Creates and returns the selectable element for a particular student entry
  */
 function createElements(student) {
-	var div = $("<div/>", { "class": "col-md-4 col-sm-3"	});
+	var div = $("<div/>", { "class": "col-md-4 col-sm-3" });
+	
+	// Make the icons, put them in their own subgrid
+	var phoneIcon = $("<span/>", { "class": "glyphicon glyphicon-earphone" });
+	var logIcon = $("<span/>", { "class": "glyphicon glyphicon-list-alt" });
+	var incidentIcon = $("<span/>", { "class": "glyphicon glyphicon-alert" });
+	
+	var icons = [phoneIcon, logIcon, incidentIcon];
+	var iconColumn = $("<div/>", {"class": "col-md-3 col-sm-3"});
+	for (icon of icons) {
+		var row = $("<div/>", {"class": "row"});
+		row.append(icon);
+		iconColumn.append(row);
+	}
+	
 	var imageSource = "../icons/index/avatar.png" // TODO: specific image for each
 	var img = $("<img>", { "class": "img-circle center-block student-photo", "src": imageSource });
+	
 	img.click(function() {
 		window.location.href = "individual-form.html";
 	});
 	var href = $("<a></a>", { "href": "individual-form.html" });
-	
 	href.append(img);
-	div.append(img);
+	var imgColumn = $("<div/>", {"class": "col-md-9, col-sm-9"}).append(href);
+	
+	div.append(imgColumn);
+	div.append(iconColumn);	
 	return div;
 }
 
