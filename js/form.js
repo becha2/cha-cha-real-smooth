20d8c8c65bc87103e4162cf18eb882695ef70ae6
Rebekah Cha <rcha@mit.edu>
2016-04-10 16:27:08 -0400

    changing css

--- js/form.js
+++ js/form.js
@@ -1,6 +1,10 @@
 
 $(function () {
-
+	// $('#result').load('ajax/test.html #container');
+	// var wanted_html = $('class-form.html .form-content');
+	// console.log(wanted_html);
+	$('.form-container').load('class-form.html');
+	// $('.form-container').html()
 	$('.section-heading').click(function(){
 		var parent = $(this).parent();
 		var icon = $(this).find('.glyphicon');
