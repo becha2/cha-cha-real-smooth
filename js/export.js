98e92d96f2f112b4c4eb307f1371c0917e0c9e41
Laura Eckman <eckmanlw@gmail.com>
2016-05-03 00:02:49 -0400

    carousel for preview

--- js/export.js
+++ js/export.js
@@ -1,6 +1,5 @@
 $(function () {
 
-    var display = ["joe"];
     var warning = {};
     
     var table = $('#student-table');
@@ -14,6 +13,46 @@ $(function () {
                 warning[row.id] = true;
             }
         }
+    } 
+
+    // creating preview
+    var classData = {};
+    classData.joe = JSON.parse(window.localStorage.getItem("Joe Desler"));
+    classData.kyle = JSON.parse(window.localStorage.getItem("Kyle Jones"));
+    classData.mandy = JSON.parse(window.localStorage.getItem("Mandy Wu"));
+    classData.sarah = JSON.parse(window.localStorage.getItem("Sarah Brown"));
+    $(".textarea").each(function() {
+        var name = $(this).attr("class").split(" ")[1];
+        var indivData = classData[name];
+        var sectionName = $(this).attr("id");
+        var formSection = sectionName.split("-")[0];
+        var info = document.createElement("p");
+        $(info).text(indivData[formSection]);
+        if (indivData[formSection] == ""){
+            warning[name] = true;
+        }
+        $(this).html(info);
+    });
+
+    setCarouselHeight('#carousel-example');
+
+    function setCarouselHeight(id)
+    {
+        var slideHeight = [];
+        $(id+' .item').each(function()
+        {
+            // add all slide heights to an array
+            slideHeight.push($(this).height());
+        });
+
+        // find the tallest item
+        max = Math.max.apply(null, slideHeight);
+
+        // set the slide's height
+        $(id+' .carousel-content').each(function()
+        {
+            $(this).css('height',max+'px');
+        });
     }
 
 
@@ -40,46 +79,15 @@ $(function () {
                 updateDisplay();
             } else {
                 this.className='';
-                removeDisplay(this.id);
-                console.log(display);
-                updateDisplay();
             }
         } else {
             this.className='active';
-            display.push(this.id);
-            console.log(display);
-            updateDisplay();
         }
         var shouldBeChecked = this.className == 'active';
         $(this).find('input:checkbox').prop('checked', shouldBeChecked);
         if (!shouldBeChecked)
             $('#checkAll').prop('checked', false);
     });
-
-    function updateDisplay(){
-        if (display.length == 0){
-            var row = $('#student-table').children()[0];
-            var firstName = row.id;
-            display.push(firstName);
-        }
-        var goalSrc = "../sampleForms/" + display[display.length-1] +  ".pdf";
-        var currentSrc = $('#pdf').children()[0].src;
-        if (currentSrc.indexOf(display[display.length-1]) == -1){   // only update if not displaying correctly
-            $('#pdf').children()[0].src = goalSrc;
-        }
-    }
-
-    function removeDisplay(name){
-        var index = display.length;
-        for (var i in display){
-            if (display[i]==name){
-                index = i;
-            }
-        }
-        if (index != display.length){
-            display.splice(index,1);
-        }
-    }
 
     $('#print').click(function () { // print confirmation popup
         $.confirm({
